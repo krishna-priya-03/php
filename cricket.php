96d61f8a7f70b3cb87745a3b11f05516a349e22b
krishna-priya-03 <kpriye74@gmail.com>
2025-11-04 13:56:56 +0530

    Add files via upload

---
+++ cricket.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <mata name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>hello world</title>
+        </head>
+        <body>
+            <?php
+            $cricket=array("virat","dhoni","rohoit","sachin");
+            echo"<br>Displaying name in table format<br/>";
+            echo"<br>...................................<br/>";
+            echo"<br>Names of cricket players are stored in table:<br/>";
+            print_r($cricket);
+            echo"<br><table border=2><tr><th>players names</th></tr><br/>";
+            echo"<tr><td>$cricket[0]</td></tr>";
+             echo"<tr><td>$cricket[1]</td></tr>";
+              echo"<tr><td>$cricket[2]</td></tr>";
+               echo"<tr><td>$cricket[3]</td></tr>";
+            echo"</table>";
+            ?>
+    </body>
+</html>
